f2488863597a67988abd0813221c9184b54cead6
Евгений Гилев <euggil@yandex.ru>
2021-09-13 20:12:38 +0500

    Добавьте файлы проекта.

---
+++ SF_ConsoleAppMod3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SF_ConsoleAppMod3
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string myName;
+            myName = "Jane11";
+
+            Console.WriteLine(myName);
+
+            Console.ReadKey();
+        }
+    }
+}
