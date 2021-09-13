28bdb396ff8a7472de9fb197e992d4db08d37d85
Евгений Гилев <euggil@yandex.ru>
2021-09-13 22:30:10 +0500

    Добавили пример с переменными разных типов

--- SF_ConsoleAppMod3/Program.cs
+++ SF_ConsoleAppMod3/Program.cs
@@ -17,6 +17,21 @@ namespace SF_ConsoleAppMod3
             Console.WriteLine("\x23");
 
             Console.ReadKey();
+
+            ///
+            /// Следующий пример, с перменными разных типов
+            /// 
+            byte MyAge = 27;
+            bool HaveIApet = true;
+            double MyShoeSize = 37.5;
+
+            Console.WriteLine("My name is " + MyName);
+            Console.WriteLine("MyAge " + MyAge);
+            Console.WriteLine("Do I have a pet? " + HaveIApet);
+            Console.WriteLine("My shoe size is " + MyShoeSize);
+
+            Console.ReadKey();
+
         }
     }
 }
