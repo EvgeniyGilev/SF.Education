195c49fd03a5d1f692dc85bce4723a1de2ae5e7d
Евгений Гилев <euggil@yandex.ru>
2021-09-17 12:14:52 +0500

    4.1. Условные выражения и конструкции- задание на цвета консоли

--- SF.Education.Mod4_ConsoleApp/Program.cs
+++ SF.Education.Mod4_ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace SF.Education.Mod4_ConsoleApp
 {
@@ -6,7 +7,58 @@ namespace SF.Education.Mod4_ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //Пример 1
+            /*
+            var a = "abc";
+            var b = "def";
+            bool c;
+            c = a != b;
+            int A;
+            int B;
+            double X;
+            double Y;
+
+            bool C = (A < B) | (X > Y);
+            */
+            var a = 6;
+            var b = 7;
+            
+            var c = (a < b) ^ (a != b);
+
+            Console.WriteLine(c);
+            Console.ReadKey();
+
+            //Пример 2 - 4.1. Условные выражения и конструкции - задание на цвета
+            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+
+            var color = Console.ReadLine();
+
+            if (color == "red")
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is red!");
+            }
+
+            else if (color == "green")
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is green!");
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is cyan!");
+                Console.ReadKey();
+            }
         }
     }
+
+    
 }
