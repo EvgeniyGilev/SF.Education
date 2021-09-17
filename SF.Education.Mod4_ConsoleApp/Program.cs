dc4fa80e05041eb0f4ccbac3ff93618952e3e270
Евгений Гилев <euggil@yandex.ru>
2021-09-17 12:22:48 +0500

    Добавили вывод цвета консоли через оператор Switch, сделали обработку ввода Cyan

--- SF.Education.Mod4_ConsoleApp/Program.cs
+++ SF.Education.Mod4_ConsoleApp/Program.cs
@@ -20,7 +20,7 @@ namespace SF.Education.Mod4_ConsoleApp
             double Y;
 
             bool C = (A < B) | (X > Y);
-            */
+          
             var a = 6;
             var b = 7;
             
@@ -28,35 +28,41 @@ namespace SF.Education.Mod4_ConsoleApp
 
             Console.WriteLine(c);
             Console.ReadKey();
-
+              */
             //Пример 2 - 4.1. Условные выражения и конструкции - задание на цвета
             Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
 
-            var color = Console.ReadLine();
-
-            if (color == "red")
+            switch (Console.ReadLine())
             {
-                Console.BackgroundColor = ConsoleColor.Red;
-                Console.ForegroundColor = ConsoleColor.Black;
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
 
-                Console.WriteLine("Your color is red!");
+                    Console.WriteLine("Your color is red!");
+                    break;
+
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is green!");
+                    break;
+                case "cyan":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is cyan!");
+                    break;
+
+                default:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    Console.WriteLine("Your color is yellow!");
+                    break;
             }
+            Console.ReadKey();
 
-            else if (color == "green")
-            {
-                Console.BackgroundColor = ConsoleColor.Green;
-                Console.ForegroundColor = ConsoleColor.Black;
-
-                Console.WriteLine("Your color is green!");
-            }
-            else
-            {
-                Console.BackgroundColor = ConsoleColor.Cyan;
-                Console.ForegroundColor = ConsoleColor.Black;
-
-                Console.WriteLine("Your color is cyan!");
-                Console.ReadKey();
-            }
         }
     }
 
