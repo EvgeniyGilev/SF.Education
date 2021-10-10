6d5a153b8658466c50a485513f897d30c7fffad8
Евгений Гилев <euggil@yandex.ru>
2021-10-10 10:57:45 +0500

    Добавили пример с ркурсией 5.5.3

---
+++ SF.Education.Mod5_ConsoleApp_RECURSION/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SF.Education.Mod5_ConsoleApp_RECURSION
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите фразу для проверки работы эхо:");
+            string phrase = Console.ReadLine();
+            Console.WriteLine("Введите глубину эхо:");
+            int deep = Int32.Parse(Console.ReadLine());
+
+            Echo(phrase, deep);
+
+            static void Echo(string saidworld, int deep)
+            {
+                var modif = saidworld;
+
+                if (modif.Length > 2)
+                {
+                    modif = modif.Remove(0, 2);
+                }
+
+                Console.WriteLine("..." + modif);
+
+                if (deep > 1)
+                {
+                    Echo(modif, deep - 1);
+                }
+
+            }
+        }
+    }
+}
