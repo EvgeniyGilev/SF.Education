e50c43bc0d722a567c4e39d45f6b644c40c142fb
Евгений Гилев <euggil@yandex.ru>
2021-10-24 13:10:14 +0500

    Изучаем модуль 8

--- SF.Education.Mod7_CondolseApp/Program.cs
+++ SF.Education.Mod7_CondolseApp/Program.cs
@@ -25,13 +25,148 @@ namespace SF.Education.Mod7_CondolseApp
 
             Console.ReadKey();
 
+            DerivedClass obj = new DerivedClass();
+            obj.Display();
+
+
+            Console.ReadKey();
+
+            D d = new D();
+            E e = new E();
+
+            d.Display();
+            ((A)e).Display();
+            ((B)d).Display();
+            ((A)d).Display();
+
+
+            Console.ReadKey();
+
+            
+
 
         }
 
 
     }
 
+    class Generic<T>
+    {
+        public T Field = default(T); // Поле типа T
 
+    }
+
+    class Record<T1,T2>
+    {
+        public T1 Id;
+        public T2 Value;
+        public DateTime Date;
+    }
+
+    class BaseClass
+    {
+        public virtual int Counter
+        {
+            get;
+            set;
+        }
+        public virtual void Display()
+        {
+            Console.WriteLine("Метод класса BaseClass");
+        }
+
+    }
+
+    class DerivedClass : BaseClass
+    {
+        private int counter;
+        public override int Counter
+        {
+
+
+            get { return counter; }
+            set
+            {
+                if (counter < 0)
+                {
+                    Console.WriteLine("Число должно быть больше 0!");
+                }
+                else
+                {
+                    counter = value;
+                }
+            }
+        }
+        public override void Display()
+        {
+            base.Display();
+            Console.WriteLine("Метод класса DerivedClass");
+        }
+
+    }
+
+    class A
+    {
+        public virtual void Display()
+        {
+            Console.WriteLine("A");
+        }
+    }
+    class B : A
+    {
+        public new void Display()
+        {
+            Console.WriteLine("B");
+        }
+    }
+    class C : A
+    {
+        public override void Display()
+        {
+            Console.WriteLine("C");
+        }
+    }
+    class D : B
+    {
+        public new void Display()
+        {
+            Console.WriteLine("D");
+        }
+    }
+    class E : C
+    {
+        public new void Display()
+        {
+            Console.WriteLine("E");
+        }
+    }
+
+    class IndexingClass
+    {
+        private int[] array;
+
+        public IndexingClass(int[] array)
+        {
+            this.array = array;
+        }
+
+        // Индексатор по массиву
+        public int this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+
+            set
+            {
+                array[index] = value;
+            }
+        }
+    }
 
 }
+
+
+
 
