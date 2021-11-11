882214b3eba8dbe1b6b6520ff2f57889fe5c35e1
Евгений Гилев <euggil@yandex.ru>
2021-11-11 23:41:07 +0500

    Добавил примеры для самоизучения по модулю 9

--- SF.Education.Mod8_ConsoleApp/Program.cs
+++ SF.Education.Mod8_ConsoleApp/Program.cs
@@ -16,11 +16,58 @@ namespace DriveManager
                 Console.WriteLine($"Тип: {drive.DriveType}");
                 if (drive.IsReady)
                 {
+
+                    
+
+
                     Console.WriteLine($"Объем: {drive.TotalSize}");
                     Console.WriteLine($"Свободно: {drive.TotalFreeSpace}");
                     Console.WriteLine($"Метка: {drive.VolumeLabel}");
                 }
             }
+            //создаем папку на диске
+            /*
+            try
+            {
+                DirectoryInfo dirInfo = new DirectoryInfo(@"С:\\" );// Или "/"  для MAС 
+                if (dirInfo.Exists)
+                {
+                    Console.WriteLine(dirInfo.GetDirectories().Length + dirInfo.GetFiles().Length);
+                }
+
+                DirectoryInfo newDirectory = new DirectoryInfo(@"/newDirectory");
+                if (!newDirectory.Exists)
+                    newDirectory.Create();
+
+                Console.WriteLine(dirInfo.GetDirectories().Length + dirInfo.GetFiles().Length);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            */
+            // сохраняем путь к файлу (допустим, вы его скачали на рабочий стол)
+            string filePath = @"C:\temp1\BinaryFile.bin";
+
+            // при запуске проверим, что файл существует
+            if (File.Exists(filePath))
+            {
+                // строковая переменная, в которую будем считывать данные
+                string stringValue;
+
+                // считываем, после использования высвобождаем задействованный ресурс BinaryReader
+                using (BinaryReader reader = new BinaryReader(File.Open(filePath, FileMode.Open)))
+                {
+                    stringValue = reader.ReadString();
+                }
+
+                // Вывод
+                Console.WriteLine("Из файла считано:");
+                Console.WriteLine(stringValue);
+
+                Console.ReadKey();
+            }
+
         }
     }
 }
---
+++ SF.Education.Mod9_ConsoleApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SF.Education.Mod9_ConsoleApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Action showMessageDelegate = ShowMessage;
+            showMessageDelegate.Invoke();
+
+            Func<int,
+                int,
+                int,
+                int> sumDelegate = Sum;
+            int result = sumDelegate.Invoke(1, 30, 120);
+            Console.WriteLine(result);
+
+            Predicate<string> checkLengthDelegate = CheckLength;
+            bool status = checkLengthDelegate.Invoke("skill_factory");
+            Console.WriteLine(status);
+
+            Console.ReadLine();
+        }
+
+        static void ShowMessage()
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static int Sum(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        static bool CheckLength(string _row)
+        {
+            if (_row.Length > 3) return true;
+            return false;
+        }
+    }
+}
